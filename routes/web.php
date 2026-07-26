6ca3452f9f18345de5e1b9cd0ff0dc103373ff5f
Laragoons <elvanfirdellia@gmail.com>
2026-07-26 12:11:39 +0700

    Routing and Controller

---
+++ routes/web.php
@@ -0,0 +1,75 @@
+<?php
+
+use App\Http\Controllers\MajorController;
+use App\Http\Controllers\SchoolClass\CreateController;
+use App\Http\Controllers\SchoolClass\DestroyController;
+use App\Http\Controllers\SchoolClass\EditController;
+use App\Http\Controllers\SchoolClass\IndexController;
+use App\Http\Controllers\SchoolClass\ShowController;
+use App\Http\Controllers\SchoolClass\StoreController;
+use App\Http\Controllers\SchoolClass\UpdateController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
+use Illuminate\Support\Facades\Route;
+ 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Teacher (ACTION)
+
+Route::name('teachers.')->prefix('teachers')->group(function() {
+    Route::get('/', [TeacherController::class, 'index'])->name('index');
+
+    Route::get('/create', [TeacherController::class, 'create'])->name('create');
+
+    Route::get('/{id}', [TeacherController::class, 'show'])->name('show');  
+
+    Route::get('/{id}/edit', [TeacherController::class, 'edit'])->name('edit'); 
+
+    Route::post('/', [TeacherController::class, 'store'])->name('store'); 
+
+    Route::put('/{id}', [TeacherController::class,'update'])->name('update');     
+
+    Route::delete('/{id}', [TeacherController::class,'destroy'])->name('destroy'); 
+});
+
+//Students (ACTION)
+
+Route::name('students.')->prefix('students')->group(function() {
+    Route::get('/', [StudentController:: class, 'index'])->name('index');
+
+    Route::get('/create', [StudentController:: class, 'create'])->name('create');
+
+    Route::get('/{id}', [StudentController:: class, 'show'])->name('show');  
+
+    Route::get('/{id}/edit', [StudentController:: class, 'edit'])->name('edit'); 
+
+    Route::post('/', [StudentController:: class, 'store'])->name('store'); 
+
+    Route::put('/{id}', [StudentController:: class, 'update'])->name('update');     
+
+    Route::delete('/{id}', [StudentController:: class, 'destroy'])->name('destroy'); 
+});
+
+//SchoolClasses (INVOKABLE)
+
+Route::name('SchoolClasses.')->prefix('SchoolClasses')->group(function() {
+    Route::get('/', IndexController::class)->name('index');
+
+    Route::get('/create', CreateController::class)->name('create');
+
+    Route::get('/{id}', ShowController::class)->name('show');  
+
+    Route::get('/{id}/edit', EditController::class)->name('edit'); 
+
+    Route::post('/', StoreController::class)->name('store'); 
+
+    Route::put('/{id}', UpdateController::class)->name('update');     
+
+    Route::delete('/{id}', DestroyController::class)->name('destroy'); 
+});
+
+//Jurusan (Resource)
+
+Route::resource('majors', MajorController::class);
